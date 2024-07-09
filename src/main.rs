7e903ed1e515f4380b18e3ac667b92adc2c86e60
grandmasponge <tobyr2006@gmail.com>
2024-07-09 12:11:35 +0100

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,122 @@ use std::env;
 use std::fs;
 use std::io::stdout;
 use std::io::{self, Write};
+use std::iter::Peekable;
 use std::process::exit;
+use std::ptr::write;
+use std::str::Chars;
 
-enum Token {
+use anyhow::Ok;
+
+struct Lexer<'a> {
+    line: i32,
+    charaters: Chars<'a>,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(contents: &'a str) -> Self {
+        let mut line = 1;
+        let chars = contents.chars();
+        Self {
+            line,
+            charaters: chars,
+        }
+    }
+
+    fn tokenize(&mut self) -> i32 {
+        let mut exitcode = 0;
+        let mut tokens = Vec::new();
+        let mut characters = self.charaters.clone().peekable();
+        while let Some(char) = characters.next() {
+            match char {
+                '\n' => {
+                    self.line += 1;
+                }
+                '(' => tokens.push(Token::newToken(
+                    TokenType::LeftParen,
+                    char.to_string(),
+                    None,
+                )),
+                ')' => tokens.push(Token::newToken(
+                    TokenType::RightParen,
+                    char.to_string(),
+                    None,
+                )),
+                '{' => tokens.push(Token::newToken(
+                    TokenType::LeftBrace,
+                    char.to_string(),
+                    None,
+                )),
+                '}' => tokens.push(Token::newToken(
+                    TokenType::RightBrace,
+                    char.to_string(),
+                    None,
+                )),
+                ',' => tokens.push(Token::newToken(TokenType::Comma, char.to_string(), None)),
+                '.' => tokens.push(Token::newToken(TokenType::Dot, char.to_string(), None)),
+                '-' => tokens.push(Token::newToken(TokenType::Minus, char.to_string(), None)),
+                '+' => tokens.push(Token::newToken(TokenType::Plus, char.to_string(), None)),
+                '*' => tokens.push(Token::newToken(TokenType::Star, char.to_string(), None)),
+                ';' => tokens.push(Token::newToken(
+                    TokenType::SemiColon,
+                    char.to_string(),
+                    None,
+                )),
+                '=' => {
+                    let mut peeker = characters.clone().peekable();
+                    if peeker.next() == Some('=') {
+                        tokens.push(Token::newToken(
+                            TokenType::EQUAL_EQUAL,
+                            "==".to_string(),
+                            None,
+                        ));
+                        characters.next();
+                    } else {
+                        tokens.push(Token::newToken(TokenType::EQUAL, char.to_string(), None));
+                    }
+                }
+                _ => {
+                    exitcode = 65;
+                    let error =
+                        TokenError::new("Error: Unexpected charater:".to_string(), self.line, 65);
+                    println!("{error}");
+                }
+            };
+        }
+        for token in tokens {
+            println!("{token}");
+        }
+        exitcode
+    }
+}
+
+struct Token {
+    _type: TokenType,
+    _string: String,
+    _value: Option<String>,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let value = match &self._value {
+            Some(value) => value,
+            None => "null",
+        };
+        write!(f, "{} {} {}", self._type, self._string, value)
+    }
+}
+
+impl Token {
+    fn newToken(_type: TokenType, _string: String, _value: Option<String>) -> Self {
+        Self {
+            _type,
+            _string,
+            _value,
+        }
+    }
+}
+
+enum TokenType {
     LeftParen,
     RightParen,
     LeftBrace,
@@ -18,9 +131,12 @@ enum Token {
     Plus,
     SemiColon,
     EOF,
+    EQUAL,
+    EQUAL_EQUAL,
     NewLine,
 }
 
+#[derive(Debug)]
 struct TokenError {
     msg: String,
     line: i32,
@@ -43,44 +159,22 @@ impl std::fmt::Display for TokenError {
     }
 }
 
-impl Token {
-    fn output(tok: char, line: i32) -> Result<Token, TokenError> {
-        match tok {
-            '(' => Ok(Token::LeftParen),
-            ')' => Ok(Token::RightParen),
-            '{' => Ok(Token::LeftBrace),
-            '}' => Ok(Token::RightBrace),
-            ',' => Ok(Token::Comma),
-            '.' => Ok(Token::Dot),
-            '-' => Ok(Token::Minus),
-            '+' => Ok(Token::Plus),
-            ';' => Ok(Token::SemiColon),
-            '*' => Ok(Token::Star),
-            _ => Err(TokenError::new(
-                format!("Error: Unexpected character: {}", tok),
-                line,
-                65,
-            )),
-        }
-    }
-
-    fn PrintOutput() {}
-}
-
-impl std::fmt::Display for Token {
+impl std::fmt::Display for TokenType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Token::LeftParen => write!(f, "LEFT_PAREN ( null"),
-            Token::RightParen => write!(f, "RIGHT_PAREN ) null"),
-            Token::LeftBrace => write!(f, "LEFT_BRACE {{ null"),
-            Token::RightBrace => write!(f, "RIGHT_BRACE }} null"),
-            Token::Comma => write!(f, "COMMA , null"),
-            Token::Dot => write!(f, "DOT . null"),
-            Token::Minus => write!(f, "MINUS - null"),
-            Token::Plus => write!(f, "PLUS + null"),
-            Token::SemiColon => write!(f, "SEMICOLON ; null"),
-            Token::Star => write!(f, "STAR * null"),
-            _ => write!(f, "EOF  null"),
+            TokenType::LeftParen => write!(f, "LEFT_PAREN"),
+            TokenType::LeftBrace => write!(f, "LEFT_BRACE"),
+            TokenType::RightParen => write!(f, "RIGHT_PAREN"),
+            TokenType::RightBrace => write!(f, "RIGHT_BRACE"),
+            TokenType::Comma => write!(f, "COMMA"),
+            TokenType::Dot => write!(f, "DOT"),
+            TokenType::Minus => write!(f, "MINUS"),
+            TokenType::Plus => write!(f, "PLUS"),
+            TokenType::SemiColon => write!(f, "SEMICOLON"),
+            TokenType::Star => write!(f, "STAR"),
+            TokenType::EQUAL => write!(f, "EQUAL"),
+            &TokenType::EQUAL_EQUAL => write!(f, "EQUAL_EQUAL"),
+            _ => write!(f, "EOF"),
         }
     }
 }
@@ -104,7 +198,9 @@ fn main() {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
-            let result = tokenize(&file_contents);
+            let mut lexer = Lexer::new(&file_contents);
+
+            let result = lexer.tokenize();
             println!("EOF  null");
             exit(result)
         }
@@ -113,23 +209,4 @@ fn main() {
             return;
         }
     }
-}
-
-fn tokenize(contents: &str) -> i32 {
-    let mut line: i32 = 1;
-    let mut exitCode = 0;
-    let chars = contents.chars();
-    for char in chars {
-        let token = Token::output(char, line);
-        match token {
-            Ok(tok) => {
-                println!("{}", tok);
-            }
-            Err(e) => {
-                writeln!(io::stderr(), "{}", e).unwrap();
-                exitCode = e.exitcode;
-            }
-        }
-    }
-    exitCode
 }
