95623b9207a061375275c7fabb059ff373c4b8b9
grandmasponge <tobyr2006@gmail.com>
2024-08-07 08:57:44 +0100

    pass stage

--- src/eval.rs
+++ src/eval.rs
@@ -63,7 +63,28 @@ impl Evaluator {
                     Value::Nil
                 }
             }
-
+            "+" => {
+                if let Value::Number(lhs) = left {
+                    let mut rhs: f32 = 0.;
+                    if let Value::Number(r) = right {
+                        rhs = r;
+                    }
+                    return Value::Number(lhs + rhs);
+                } else {
+                    Value::Nil
+                }
+            }
+            "-" => {
+                if let Value::Number(lhs) = left {
+                    let mut rhs: f32 = 0.;
+                    if let Value::Number(r) = right {
+                        rhs = r;
+                    }
+                    return Value::Number(lhs - rhs);
+                } else {
+                    Value::Nil
+                }
+            }
             _ => unreachable!(),
         }
     }
